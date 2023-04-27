21c682e7af7613a07428ffb1868a31bf66d47b05
Mark Dawson <markgdawson@gmail.com>
2023-04-27 08:01:29 +0100

    Update sof-firmware to 2.2.4

--- nongnu/packages/linux.scm
+++ nongnu/packages/linux.scm
@@ -970,7 +970,7 @@ documented in the respective processor revision guides.")
 (define-public sof-firmware
   (package
     (name "sof-firmware")
-    (version "2.5")
+    (version "2.2.4")
     (source
      (origin
        (method url-fetch)
